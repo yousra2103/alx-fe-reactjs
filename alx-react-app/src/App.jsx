bd645fa77805d8e002ccb1e9a569d6fb0327b1cb
Yousr <youssrabenlimam88@gmail.com>
2024-08-09 22:45:40 +0100

    adding files 5

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -34,8 +35,9 @@ function App() {
       </p>
       <WelcomeMessage />
       <Header/>
-      <Footer/>
       <MainContent/>
+      <Footer/>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   )
 }
