e4437f34c7604171622448758c12c0242ec50bed
Yousr <youssrabenlimam88@gmail.com>
2024-08-09 22:13:42 +0100

    adding files 323

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './WelcomeMessage'
-import Header from './Header'
-import MainContent from './MainContent'
-import Footer from './Footer'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
