c82b140009f924606e05e782ea0d3fa4af0215a3
Benlimam yousra <youssrabenlimam88@gmail.com>
2024-08-22 23:23:30 +0100

    TRYING TO REINSTALL GIT

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        wa tQaaaaaaaaaaaaad al MeerDDDD !!
+      </p>
+      <Header />
+      <WelcomeMessage />
+      <MainContent />
+      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </>
+  )
+}
+
+export default App
---
+++ alx-react-app-props/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header(){
+   return(
+    <div>
+    <header>
+<h1>My Favorite Cities</h1>
+</header>
+</div>
+   );
+
+}
+
+export default Header ;
