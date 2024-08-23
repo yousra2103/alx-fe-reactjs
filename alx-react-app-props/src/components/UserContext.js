d131efaf1759690512f0e7e0f9bd3c0cf728d309
Benlimam yousra <youssrabenlimam88@gmail.com>
2024-08-23 08:26:31 +0100

    ADDING NEW FILES

---
+++ alx-react-app-props/src/components/UserContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// Create the context
+const MyContext = React.createContext();
+
+// Export the context
+export default MyContext;
--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,10 +1,7 @@
-const UserProfile = (props) => {
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
-  export default UserProfile ;
+import UserInfo from './UserInfo';
+
+function ProfilePage({ userData }) {
+  return <UserInfo userData={userData} />;
+}
+
+export default ProfilePage;
