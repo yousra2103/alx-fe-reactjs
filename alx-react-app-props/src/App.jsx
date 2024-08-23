58ffc4ee23effd8ec10f6a8c7e3f718c08abf0fc
Benlimam yousra <youssrabenlimam88@gmail.com>
2024-08-23 08:38:04 +0100

    FIXING APP

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,13 +3,15 @@ import { useState } from 'react'
 import UserProfile from './components/UserProfile'
 
 function App() {
-  
+  const [state, setState] = useState('some value');
     const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <>
       
       <ProfilePage userData={userData} />;
+      <MyContext.Provider value={{ state, setState }} />
+      <MyContext.Provider />
       
      
     </>
