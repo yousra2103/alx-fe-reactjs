eb67adf4b2e5b8339936ebe7cd3f09f7042ad4a9
Benlimam yousra <youssrabenlimam88@gmail.com>
2024-08-25 13:53:00 +0100

    zustand app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <RecipeList />
+      <AddRecipeForm />
+    </>
+  )
+}
+
+export default App
