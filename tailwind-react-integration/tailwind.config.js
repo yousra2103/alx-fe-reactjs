ae11dc716d0bd2224b202e834e2f624e2e97f2e6
Benlimam yousra <youssrabenlimam88@gmail.com>
2024-09-08 21:52:52 +0100

    tailwind

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: false,
+  variants: { extend: {} },
+}
+
+
+
