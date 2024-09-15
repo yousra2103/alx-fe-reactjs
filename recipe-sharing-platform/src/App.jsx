0741d46ce05bb2dfb7436bcd9ac1cdd73b4664cc
Benlimam yousra <youssrabenlimam88@gmail.com>
2024-09-15 16:27:48 +0100

    tailwind project

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+     
+      <p className="text-blue-500">
+       yuyhjggfkhf
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  plugins: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  }
+}
+
