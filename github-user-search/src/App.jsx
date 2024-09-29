ccee2a6d921b91e0fbeef384c9ddb517ac347cf2
Benlimam yousra <youssrabenlimam88@gmail.com>
2024-09-29 18:59:45 +0100

    start project

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,11 +1,30 @@
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
  
 
   return (
     <>
-     
+     <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/ABOUT">ABOUT</a></li>
+            <li><a href="/Contact">Contact</a></li>
+          </ul>
+        </nav>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+     </Router>
     </>
   )
 }
